003404c6f184adaef72313ff62df7032f78b7a24
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-12-04 21:22:56 +0100

    Add files via upload

---
+++ KlasaX_priprema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaX_priprema
+{
+    class KlasaX
+    {
+        private int broj;
+
+        public int Broj { get => broj; set => broj = value; }
+        
+        public KlasaX(int broj1) 
+        {
+            broj = broj1;
+        }
+        public override string ToString()
+        {
+            return "vrijednost varijable broj je " + broj;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX broj = new KlasaX(10);
+            Console.WriteLine(broj);
+            Console.ReadKey();
+        }
+    }
+}
